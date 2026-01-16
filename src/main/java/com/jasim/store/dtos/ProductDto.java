219ade81b0f8fa05fe5ffc923a62019426a8a61a
Jasim Chouhan <jasim.chouhan@yahoo.com>
2026-01-16 17:05:01 +0400

    description added

--- src/main/java/com/jasim/store/dtos/ProductDto.java
+++ src/main/java/com/jasim/store/dtos/ProductDto.java
@@ -14,6 +14,7 @@ import java.math.BigDecimal;
 public class ProductDto {
     private Long id;
     private String name;
+    private String description;
     private BigDecimal price;
     private Short categoryId;
 }
--- src/main/java/com/jasim/store/entities/Product.java
+++ src/main/java/com/jasim/store/entities/Product.java
@@ -25,6 +25,9 @@ public class Product {
     @Column(name = "name")
     private String name;
 
+    @Column(name = "description")
+    private String description;
+
     @Column(name = "price", precision = 10, scale = 2)
     private BigDecimal price;
 
